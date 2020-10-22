a0b9c8d91f2cf5abefc424f27775813dfd67c926
taeboki <tae@Tae.localdomain>
2020-10-22 22:51:19 +0800

    Oct22 Commit

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,26 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello, My name is Darian.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 class App extends Component {
   constructor() {
     super();
